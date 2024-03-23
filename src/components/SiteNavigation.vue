0ff067178b8773da6996944bbb81b2f1063d1608
Negin Mashhadi <nmashhad@uci.edu>
2024-03-22 21:12:24 -0700

    initial files

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,60 @@
+<template>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+      <RouterLink :to="{ name: 'home-page' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-cloud text-2xl"></i>
+          <p class="text-2xl">Your Local Weather</p>
+        </div>
+      </RouterLink>
+
+      <div class="flex gap-3 flex-1 justify-end">
+        <i
+          class="fa-solid fa-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+        ></i>
+        <i
+          class="fa-solid fa-list text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+        ></i>
+      </div>
+      <BaseModal>
+        <div class="text-black">
+          <h1 class="text-2xl mb-1">About:</h1>
+          <p class="mb-4">
+            The Local Weather allows you to track the current and future weather
+            of cities of your choosing.
+          </p>
+          <h2 class="text-2xl">How it works:</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>
+              Search for your city by entering the name into the search bar.
+            </li>
+            <li>
+              Select a city within the results, this will take you to the
+              current weather for your selection.
+            </li>
+            <li>
+              Find the city by clicking on the menu icon on the top right.
+              Select a city to view at a later time on the home page.
+            </li>
+          </ol>
+
+          <h2 class="text-2xl">Removing a city</h2>
+          <p>
+            If you no longer wish to track a city, simply select the city within
+            the home page. At the bottom of the page, there will be am option to
+            remove the city from you list.
+          </p>
+        </div>
+      </BaseModal>
+    </nav>
+  </header>
+</template>
+
+
+<script setup>
+import { RouterLink } from "vue-router";
+import BaseModal from "./BaseModal.vue";
+</script>
+
